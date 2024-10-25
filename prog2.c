9629f9e53861a6e55514be4fa55d7fcf0431134a
vishal Kumar <128027462+vishalkumar9199@users.noreply.github.com>
2024-10-25 21:20:32 +0530

    Add files via upload
    Sum of the two number.

---
+++ prog2.c
@@ -0,0 +1,14 @@
+// Sum of the two number
+
+#include<stdio.h>
+int main(){
+    
+    int a,b;
+    printf("Enter a:");
+    scanf("%d",&a);
+
+    printf("Enter b:");
+    scanf("%d",&b);
+
+    printf("Sum is: %d",  a + b);
+}
