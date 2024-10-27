f43525176e8674e0aa75e16578fc02b73dc6f911
vishal Kumar <128027462+vishalkumar9199@users.noreply.github.com>
2024-10-27 23:28:30 +0530

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -1,14 +1,1 @@
-// Sum of the two number
 
-#include<stdio.h>
-int main(){
-    
-    int a,b;
-    printf("Enter a:");
-    scanf("%d",&a);
-
-    printf("Enter b:");
-    scanf("%d",&b);
-
-    printf("Sum is: %d",  a + b);
-}
