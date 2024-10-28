1738c00280fa246226460e4fc2083bfc12b8e7f8
vishal Kumar <128027462+vishalkumar9199@users.noreply.github.com>
2024-10-28 11:57:30 +0530

    Update prime_no.c

--- prime_no.c
+++ prime_no.c
@@ -1,1 +1,28 @@
+#include  <stdio.h> 
+#include  <math.h>  
+int main() {
+    int n, i;
+    int isPrime = 1; 
 
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        isPrime = 0; 
+    } else {
+        for (i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrime = 0;
+                break;  
+            }
+        }
+    }
+
+    if (isPrime == 1) {
+        printf("%d is a Prime number\n", n);
+    } else {
+        printf("%d is not a Prime number\n", n);
+    }
+
+    return 0;
+}
